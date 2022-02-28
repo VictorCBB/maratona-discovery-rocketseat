41d5600984f66ba88c6a8dc3abbbfc888c907452
Victor Barros <victorclovisb@hotmail.com>
2022-02-28 19:50:23 -0300

    Added the new file script.js and link this code with index.html

---
+++ src/script.js
@@ -0,0 +1,18 @@
+const Modal = {
+    open() {
+        //abrir modal
+        //adicionar a classe active ao modal
+        document
+            .querySelector(".modal-overlay")
+            .classList
+            .add('active');
+    },
+    close(){
+        //fechar modal
+        //remover a classe active do modal
+        document
+            .querySelector(".modal-overlay")
+            .classList
+            .remove('active');
+    }
+} 
